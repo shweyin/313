cee7a16ca7e609e564337059beb1b14cc4284d70
Shweyin <shweyin@gmail.com>
2023-12-25 00:31:56 -0500

    added start screen animation

--- src/app/components/StartScreen.js
+++ src/app/components/StartScreen.js
@@ -8,28 +8,33 @@ const StartScreen = () => {
   const [showStartScreen, setShowStartScreen] = useState(true);
   return (
     <div>
-      {showStartScreen && (
-        <div
-          className="absolute bg-top inset-0 z-10 text-center"
-          onClick={() => setShowStartScreen(false)}
+      <div
+        className={`absolute flex flex-col justify-center bg-top inset-0 z-10 text-center cursor-pointer duration-300 ${
+          showStartScreen ? "translate-y-0" : "translate-y-[-100%]"
+        }`}
+        onClick={() => setShowStartScreen(false)}
+      >
+        <Image
+          className="mt-0 mb-o mr-auto ml-auto p-4 w-96"
+          src={sign}
+          width={500}
+          height={500}
+          alt="sign"
+        />
+        <Image
+          className="mt-0 mb-o mr-auto ml-auto w-36"
+          src={santa}
+          alt="my gif"
+          height={500}
+          width={500}
+        />
+        <button
+          type="button"
+          className="pt-5 text-top-button font-medium animate-bounce "
         >
-          <Image
-            className="mt-0 mb-o mr-auto ml-auto p-4 w-96"
-            src={sign}
-            width={500}
-            height={500}
-            alt="sign"
-          />
-          <Image
-            className="mt-0 mb-o mr-auto ml-auto w-36"
-            src={santa}
-            alt="my gif"
-            height={500}
-            width={500}
-          />
-          <button type="button" className="text-top-button font-medium">Tap to Start...</button>
-        </div>
-      )}
+          Tap to Start...
+        </button>
+      </div>
     </div>
   );
 };
