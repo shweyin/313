8cb994745d3678e5a961341c461a258a44601f14
Shweyin <shweyin@gmail.com>
2024-05-12 10:55:00 -0400

    remove button, dynamics text color

--- components/Game.tsx
+++ components/Game.tsx
@@ -12,11 +12,11 @@ import Sky from "../assets/sky.jpg";
 // ];
 
 const colors = [
-  "bg-slate-300",
-  "bg-slate-400",
-  "bg-slate-500",
-  "bg-slate-600",
-  "bg-slate-700",
+  ["bg-slate-300", "text-black"],
+  ["bg-slate-400", "text-black"],
+  ["bg-slate-500", "text-white"],
+  ["bg-slate-600", "text-white"],
+  ["bg-slate-700", "text-white"],
 ];
 
 const BLANK_GAME: Score[] = [
@@ -60,6 +60,13 @@ export default function Game() {
         score: ["", "", "", "", "", "", "", "", "", "", ""],
       },
     ]);
+  };
+
+  const removePlayer = (index: number) => {
+    if (scores.length === 1) return;
+    const newGamestate = [...scores];
+    newGamestate.splice(index, 1);
+    setGamestate(newGamestate);
   };
 
   const clearScores = () => {
@@ -151,9 +158,13 @@ export default function Game() {
                     <input
                       onChange={handleNameChange}
                       id={`${playerIndex}`}
-                      value={scores[playerIndex].name}
+                      // value={scores[playerIndex].name}
+                      value={"New Player"}
                       className="bg-transparent rounded-sm p-1  focus:bg-slate-700 outline-none input input-bordered input-xs w-full max-w-xs"
                       tabIndex={playerIndex + 1}
+                      onFocus={(event) => {
+                        event.target.select();
+                      }}
                     />
                   </th>
                   {player.score.map((score: any, round: any) => (
@@ -161,7 +172,9 @@ export default function Game() {
                       <input
                         tabIndex={(round + 1) * scores.length + playerIndex + 1}
                         className={`${
-                          colors[playerIndex % 5]
+                          colors[playerIndex % 5][0] +
+                          " " +
+                          colors[playerIndex % 5][1]
                         } focus:opacity-80 rounded-sm p-1 text-white focus:outline-white input input-bordered input-xs w-full max-w-xs`}
                         id={`${playerIndex}-${round}`}
                         value={scores[playerIndex].score[round]}
@@ -170,8 +183,29 @@ export default function Game() {
                       />
                     </td>
                   ))}
-                  <th>{getScoreSum(player.score)}</th>
-                  <th>{getPlaceNumber(player)}</th>
+                  <th className="text-center">{getScoreSum(player.score)}</th>
+                  <th className="text-center">{getPlaceNumber(player)}</th>
+                  <th>
+                    <button
+                      className=""
+                      onClick={() => removePlayer(playerIndex)}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                        />
+                      </svg>
+                    </button>
+                  </th>
                 </tr>
               ))}
             </tbody>
